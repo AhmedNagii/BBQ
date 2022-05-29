ce47b4eb241f3da63c2fc7ff8aaf79052e7eca3b
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-05-29 12:21:53 +0200

    make the page interactive
    use user input tp personalize the response

---
+++ index.js
@@ -0,0 +1,19 @@
+let emailCollecterForm =document.getElementById('email-collector');
+emailCollecterForm.addEventListener('submit', event =>{
+    event.preventDefault();
+
+    let ourFormData = new FormData(event.target);   
+    let userFirstName = ourFormData.get('firstName');
+    let userEmailAddress = ourFormData.get('emailAddress');
+    let updatedHtml = `
+    <h2>Congratulations, ${userFirstName}!</h2>
+    <p>You're on your way to becoming a BBQ Master!</p>
+    <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>
+    <p class="fine-print">
+    We'll never share your information without your permission</p>
+    `;
+
+    let updatedMainContent = document.getElementById('Main-Content');
+
+    updatedMainContent.innerHTML = updatedHtml;
+})
